eb12e0cb44becdcd34ec6513abf4cef0f788ec17
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2023-11-17 17:26:56 +0100

    feat: fine

---
+++ main.js
@@ -0,0 +1,70 @@
+'use strict';
+
+/*
+<div class="items">
+  <div class="prev"></div>
+  <div class="item active">
+    <img src="img/01.jpg" alt="img01" />
+  </div>
+  <div class="item">
+    <img src="img/02.jpg" alt="img02" />
+  </div>
+  <div class="item">
+    <img src="img/03.jpg" alt="img03" />
+  </div>
+  <div class="item">
+    <img src="img/04.jpg" alt="img04" />
+  </div>
+  <div class="item">
+    <img src="img/05.jpg" alt="img05" />
+  </div>
+  <div class="next"></div>
+</div>;
+*/
+
+const imag = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+const items = document.querySelector('.items');
+let currentItem = 0;
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
+for (let i = 0; i < imag.length; i++) {
+  const item = document.createElement('div');
+  item.classList.add('item');
+
+  if (i === currentItem) {
+    item.classList.add('active');
+  }
+
+  const img = document.createElement('img');
+  img.src = `img/${imag[i]}`;
+  img.alt = `img${i}`;
+
+  item.append(img);
+  items.append(item);
+}
+
+const listItem = document.querySelectorAll('.item');
+
+prev.addEventListener('click', function () {
+  console.log('clicco su foto precedente');
+
+  listItem[currentItem].classList.remove('active');
+  currentItem = (currentItem - 1 + listItem.length) % listItem.length;
+  listItem[currentItem].classList.add('active');
+});
+
+next.addEventListener('click', function () {
+  console.log('clicco su foto successiva');
+
+  listItem[currentItem].classList.remove('active');
+  currentItem = (currentItem + 1) % listItem.length;
+  console.log(currentItem);
+  listItem[currentItem].classList.add('active');
+});
+
+setInterval(function () {
+  listItem[currentItem].classList.remove('active');
+  currentItem = (currentItem + 1) % listItem.length;
+  listItem[currentItem].classList.add('active');
+}, 3000);
